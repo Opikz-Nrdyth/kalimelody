11b57fb0eee3bd5237c5d72306e574730355f807
Opikzz <tofiqnurhidayat110@gmail.com>
2025-06-09 21:40:20 +0700

    update pesan draf

--- index.php
+++ index.php
@@ -130,7 +130,12 @@
                     <?php else: ?>
                         <?php foreach ($songs_to_display as $song): ?>
                             <tr class="border-t border-slate-200 dark:border-slate-700 cursor-pointer hover:bg-gray-200 dark:hover:bg-slate-700/50">
-                                <td class="p-4 font-semibold whitespace-nowrap"><a href="viewer.php?song=<?= str_replace([".json", "tab_"], ["", ""], urlencode($song['filename'])) ?>"><?= $song['title'] ?></a></td>
+                                <td class="p-4 font-semibold whitespace-nowrap">
+                                    <a href="viewer.php?song=<?= str_replace([".json", "tab_"], ["", ""], urlencode($song['filename'])) ?>"><?= $song['title'] ?></a>
+                                    <?php if ($song['status'] === 'draf'): ?>
+                                        <span class="ml-2 text-xs font-semibold bg-gray-200 text-gray-700 px-2 py-1 rounded-full align-middle">Draf</span>
+                                    <?php endif; ?>
+                                </td>
                                 <td class="p-4 flex justify-center items-center gap-4 text-slate-600 dark:text-slate-400">
                                     <a href="index.php?action=backup_song&file=<?= urlencode($song['filename']) ?>" class="text-transparent backup-btn whitespace-nowrap">
                                         <i class="fas fa-save mr-1"></i>Backup
